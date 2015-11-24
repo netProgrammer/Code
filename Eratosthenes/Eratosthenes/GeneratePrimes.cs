561a289452ea69a2b608f39717f0b48589c97bc1
netProgrammer <vova@programmer.net>
2015-11-24 10:05:21 +0200

    before Refactoring

--- Eratosthenes/Eratosthenes/GeneratePrimes.cs
+++ Eratosthenes/Eratosthenes/GeneratePrimes.cs
@@ -1,6 +1,56 @@
-﻿namespace Eratosthenes
+﻿using System;
+
+namespace Eratosthenes
 {
     public class GeneratePrimes
     {
+        public static int[] GeneratePrimeNumbers(int maxValue)
+        {
+            if (maxValue >= 2)
+            {
+                int arraySize = maxValue + 1;
+                bool[] workArray = new bool[arraySize];
+
+                for (int i = 0; i < arraySize; i++)
+                {
+                    workArray[i] = true;
+                }
+
+                workArray[0] = workArray[1] = false; //exclude not prime numbers
+
+                for (int j = 0; j < Math.Sqrt(arraySize) + 1; j++)
+                {
+                    if (workArray[j])
+                    {
+                        for (int i = 2*j; i < arraySize; i+=j)
+                        {
+                            workArray[i] = false;
+                        }
+                    }
+                }
+
+                int count = 0;
+                for (int i = 0; i < arraySize; i++)
+                {
+                    if (workArray[i])
+                    {
+                        count++;
+                    }
+                }
+
+                int[] result = new int[count];
+                for (int j = 0, i = 0; j < arraySize; j++)
+                {
+                    if (workArray[j])
+                    {
+                        result[i++] = j;
+                    }
+                }
+
+                return result;
+            }
+
+            return new int[0]; //maxValue < 2
+        }
     }
 }
