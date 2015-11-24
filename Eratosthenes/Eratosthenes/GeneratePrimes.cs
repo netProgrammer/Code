ebc34bab4c7d436d320f49408b0a9c9a153e8f36
netProgrammer <vova@programmer.net>
2015-11-24 10:37:56 +0200

    Rework

--- Eratosthenes/Eratosthenes/GeneratePrimes.cs
+++ Eratosthenes/Eratosthenes/GeneratePrimes.cs
@@ -1,56 +1,61 @@
 ﻿using System;
+using System.Linq;
 
 namespace Eratosthenes
 {
     public class GeneratePrimes
     {
+        private static bool[] _workArray;
+        private static int[] _result;
+
         public static int[] GeneratePrimeNumbers(int maxValue)
         {
-            if (maxValue >= 2)
+            if (maxValue < 2)
             {
-                int arraySize = maxValue + 1;
-                bool[] workArray = new bool[arraySize];
-
-                for (int i = 0; i < arraySize; i++)
-                {
-                    workArray[i] = true;
-                }
-
-                workArray[0] = workArray[1] = false; //exclude not prime numbers
-
-                for (int j = 0; j < Math.Sqrt(arraySize) + 1; j++)
-                {
-                    if (workArray[j])
-                    {
-                        for (int i = 2*j; i < arraySize; i+=j)
-                        {
-                            workArray[i] = false;
-                        }
-                    }
-                }
-
-                int count = 0;
-                for (int i = 0; i < arraySize; i++)
-                {
-                    if (workArray[i])
-                    {
-                        count++;
-                    }
-                }
-
-                int[] result = new int[count];
-                for (int j = 0, i = 0; j < arraySize; j++)
-                {
-                    if (workArray[j])
-                    {
-                        result[i++] = j;
-                    }
-                }
-
-                return result;
+                return new int[0];
             }
 
-            return new int[0]; //maxValue < 2
+            InitArray(maxValue);
+            Sieve();
+            LoadPrimes();
+            return _result;
+        }
+
+        private static void LoadPrimes()
+        {
+            _result = new int[_workArray.Count(t => t)];
+
+            for (int j = 0, i = 0; j < _workArray.Length; j++)
+            {
+                if (_workArray[j])
+                {
+                    _result[i++] = j;
+                }
+            }
+        }
+
+        private static void Sieve()
+        {
+            for (var j = 0; j < Math.Sqrt(_workArray.Length) + 1; j++)
+            {
+                if (_workArray[j])
+                {
+                    for (var i = 2*j; i < _workArray.Length; i += j)
+                    {
+                        _workArray[i] = false;
+                    }
+                }
+            }
+        }
+
+        private static void InitArray(int maxValue)
+        {
+            _workArray = new bool[maxValue + 1];
+            _workArray[0] = _workArray[1] = false; //exclude not prime numbers
+            for (var i = 2; i < _workArray.Length; i++)
+            {
+                _workArray[i] = true;
+            }
         }
     }
 }
